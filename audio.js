76c953e96226e9e2c52d1a699feba9987f0b7e03
Nana Amponsah <okinenana9@gmail.com>
2024-03-02 11:02:28 +0000

    My first advanced website

---
+++ audio.js
@@ -0,0 +1,393 @@
+const alphabet = [
+    {
+        name:"A", 
+        audio: ""
+    },
+    {
+        name:"B", 
+        audio: ""
+    },
+    {
+        name:"C", 
+        audio: ""
+    },    
+    {
+        name:"D", 
+        audio: ""
+    },
+    {
+        name:"E", 
+        audio: ""
+    },   
+    {
+        name:"F", 
+        audio: ""
+    },   
+    {
+        name:"G", 
+        audio: ""
+    },   
+    {
+        name:"H", 
+        audio: ""
+    },   
+    {
+        name:"I", 
+        audio: ""
+    },   
+    {
+        name:"J", 
+        audio: ""
+    },   
+    {
+        name:"K", 
+        audio: ""
+    },   
+    {
+        name:"L", 
+        audio: ""
+    },      
+     {
+        name:"M", 
+        audio: ""
+    },      
+     {
+        name:"N", 
+        audio: ""
+    },
+    {
+        name:"O", 
+        audio: ""
+    },       
+    {
+        name:"P", 
+        audio: ""
+    },       
+    
+    {
+        name:"Q", 
+        audio: ""
+    },       
+    {
+        name:"R", 
+        audio: ""
+    },    
+    {
+        name:"S", 
+        audio: ""
+    },      
+    {
+        name:"T", 
+        audio: ""
+    },       
+    {
+        name:"U", 
+        audio: ""
+    },       
+    {
+        name:"V", 
+        audio: ""
+    },       
+    {
+        name:"W", 
+        audio: ""
+    },       
+    {
+        name:"X", 
+        audio: ""
+    },       
+    {
+        name:"Y", 
+        audio: ""
+    },       
+    {
+        name:"Z", 
+        audio: ""
+    },       
+]
+const bodyparts = [
+    {
+        name:"Arm", 
+        audio: ""
+    },
+    {
+        name:"Ear", 
+        audio: ""
+    },       
+    {
+        name:"Eye", 
+        audio: ""
+    },       
+    {
+        name:"Finger", 
+        audio: ""
+    },       
+    {
+        name:"Foot", 
+        audio: ""
+    },       
+    {
+        name:"Head", 
+        audio: ""
+    },       
+    {
+        name:"Leg", 
+        audio: ""
+    },       
+    {
+        name:"Mouth", 
+        audio: ""
+    },       
+    {
+        name:"Nose", 
+        audio: ""
+    },       
+    {
+        name:"Stomach", 
+        audio: ""
+    },   
+]
+const wildanimals = [
+    {
+        name:"Bear", 
+        audio: ""
+    },
+    {
+        name:"Crocodile", 
+        audio: ""
+    },       
+    {
+        name:"Deer", 
+        audio: ""
+    },       
+    {
+        name:"Fox", 
+        audio: ""
+    },       
+    {
+        name:"Lion", 
+        audio: ""
+    },       
+    {
+        name:"Monkey", 
+        audio: ""
+    },       
+    {
+        name:"Rhino", 
+        audio: ""
+    },       
+    {
+        name:"Snake", 
+        audio: ""
+    },       
+    {
+        name:"Tiger", 
+        audio: ""
+    },       
+    {
+        name:"Zebra", 
+        audio: ""
+    },   
+]
+const domesticanimals = [
+    {
+        name:"Camel", 
+        audio: ""
+    },
+    {
+        name:"Cat", 
+        audio: ""
+    },       
+    {
+        name:"Dog", 
+        audio: ""
+    },       
+    {
+        name:"Duck", 
+        audio: ""
+    },       
+    {
+        name:"Goat", 
+        audio: ""
+    },       
+    {
+        name:"Hen", 
+        audio: ""
+    },       
+    {
+        name:"Horse", 
+        audio: ""
+    },       
+    {
+        name:"Parrot", 
+        audio: ""
+    },       
+    {
+        name:"Rabbit", 
+        audio: ""
+    },       
+    {
+        name:"Sheep", 
+        audio: ""
+    },   
+]
+const fruits = [
+    {
+        name:"Apple", 
+        audio: ""
+    },
+    {
+        name:"Banana", 
+        audio: ""
+    },       
+    {
+        name:"Grape", 
+        audio: ""
+    },       
+    {
+        name:"Kiwi", 
+        audio: ""
+    },       
+    {
+        name:"Mango", 
+        audio: ""
+    },       
+    {
+        name:"Orange", 
+        audio: ""
+    },       
+    {
+        name:"Pawpaw", 
+        audio: ""
+    },       
+    {
+        name:"Pear", 
+        audio: ""
+    },       
+    {
+        name:"Pineapple", 
+        audio: ""
+    },       
+    {
+        name:"Strawberry", 
+        audio: ""
+    },   
+]
+const vegetables = [
+    {
+        name:"Broccoli", 
+        audio: ""
+    },
+    {
+        name:"Cabbage", 
+        audio: ""
+    },       
+    {
+        name:"Carrot", 
+        audio: ""
+    },       
+    {
+        name:"Cucumber", 
+        audio: ""
+    },       
+    {
+        name:"Green,Red Pepper", 
+        audio: ""
+    },       
+    {
+        name:"Lettuce", 
+        audio: ""
+    },       
+    {
+        name:"Mashroom", 
+        audio: ""
+    },       
+    {
+        name:"Onion", 
+        audio: ""
+    },       
+    {
+        name:"Potato", 
+        audio: ""
+    },       
+    {
+        name:"Tomato", 
+        audio: ""
+    },   
+]
+const numbers = [
+    {
+        name:"One", 
+        audio: ""
+    },
+    {
+        name:"Two", 
+        audio: ""
+    },       
+    {
+        name:"Three", 
+        audio: ""
+    },       
+    {
+        name:"Four", 
+        audio: ""
+    },       
+    {
+        name:"Five", 
+        audio: ""
+    },       
+    {
+        name:"Six", 
+        audio: ""
+    },       
+    {
+        name:"Seven", 
+        audio: ""
+    },       
+    {
+        name:"Eight", 
+        audio: ""
+    },       
+    {
+        name:"Nine", 
+        audio: ""
+    },       
+    {
+        name:"Ten", 
+        audio: ""
+    },   
+]
+const planets = [
+    {
+        name:"Mercury", 
+        audio: ""
+    },
+    {
+        name:"Venus", 
+        audio: ""
+    },       
+    {
+        name:"Earth", 
+        audio: ""
+    },       
+    {
+        name:"Mars", 
+        audio: ""
+    },       
+    {
+        name:"Jupiter", 
+        audio: ""
+    },       
+    {
+        name:"Saturn", 
+        audio: ""
+    },       
+    {
+        name:"Uranus", 
+        audio: ""
+    },       
+    {
+        name:"Neptune", 
+        audio: ""
+    },       
+]
